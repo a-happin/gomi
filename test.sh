b2ddcf8fce838409e3a604ade494a268913bee96
a_happin <10003601+a@users.noreply.github.com>
2022-01-28 22:57:28 +0900

    Add test

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+f() {
+  clang++ -std=c++2b -Wall -Wextra --pedantic-errors -I./include -I./third-party/iutest/include -O2 -pipe -o /tmp/a.out "$i" || return
+  /tmp/a.out || return
+  g++ -std=c++2b -Wall -Wextra --pedantic-errors -I./include -I./third-party/iutest/include -O2 -pipe -o /tmp/a.out "$i" || return
+  /tmp/a.out || return
+}
+
+if [[ $# -eq 0 ]]
+then
+  set -- $(fd --hidden --exclude .git '\.cpp$' test)
+fi
+
+for i in "$@"
+do
+  printf 'Testing %s ...\n' "$i"
+  f "$i"
+done
--- test/utf8/validated_u8string_view.cpp
+++ test/utf8/validated_u8string_view.cpp
@@ -1,21 +1,23 @@
 #include <chino/utf8.hpp>
-#include <gtest/gtest.h>
 
-TEST (substrTest, valid)
+#define IUTEST_USE_MAIN
+#include <iutest.hpp>
+
+IUTEST (substrTest, valid)
 {
   using namespace chino::utf8::literals;
   {
     chino::utf8::valid_u8string_view str {u8"This is a pen"_sv};
     auto ret1 = str.substr_in_bytes (5);
     auto ret2 = str.substr_in_bytes (5, 2);
-    EXPECT_EQ (ret1, u8"is a pen"_sv);
-    EXPECT_EQ (ret2, u8"is"_sv);
+    IUTEST_EXPECT (ret1 == u8"is a pen"_sv);
+    IUTEST_EXPECT (ret2 == u8"is"_sv);
   }
 }
 
-TEST (substrTest, invalid)
+IUTEST (substrTest, invalid)
 {
   using namespace chino::utf8::literals;
-  EXPECT_ANY_THROW (u8"あ"_sv.substr_in_bytes (1));
+  IUTEST_EXPECT_ANY_THROW(u8"あ"_sv.substr_in_bytes (1));
 }
 
